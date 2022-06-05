2a7f73b27ef6c2f4ffcb071d70bcc820ec9395cb
kevinjunge <k.vinjunge@gmail.com>
2022-06-04 17:07:18 -0700

    get browser to popup init

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from selenium import webdriver
+import json
 
 class JobApply:
 
     def __init__(self, data):
         """Parameter initialization"""
+        
+        self.email = data['email']
+        self.password = data['password']
+        self.keywords = data['keywords']
+        self.location = data['location']
+        self.driver_path = webdriver.Chrome(data['driver_path'])
 
 
+if __name__ == "__main__":
+    
+    with open('config.json') as config_file:
+        data = json.load(config_file)
 
-if __name__ = "__main__":
-    bot = JobApply()
+
+    bot = JobApply(data)
+
