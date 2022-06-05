221041a10df9959d90aba6fc927079ee3d38388d
kevinjunge <k.vinjunge@gmail.com>
2022-06-05 16:00:51 -0700

    go to job section

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import json
-
+import time
 class JobApply:
 
     def __init__(self, data):
@@ -29,7 +29,22 @@ class JobApply:
         login_password.send_keys(self.password)
         login_password.send_keys(Keys.RETURN)
 
+    def job_search(self):
+        # this function goes to the 'Jobs' section and looks for all the jobs that
+        # matches the keywords and location
+        # 
+        # go to jobs section:
+        jobs_link = self.driver.find_element_by_link_text('Jobs')
+        jobs_link.click()
 
+        # introduce our keyword and location and hit enter
+        search_keyword = self.driver.find_element_by_xpath("//input[starts-with(@id, 'jobs-search-box-keyword']")
+        search_keyword.clear()
+        search_keyword.send_keys(self.keyword)
+        search_location = self.driver.find_element_by_xpath("//input[starts-with(@id, 'jobs-search-box-location']")
+        search_location.clear()
+        search_location.send_keys(self.location)
+         
 
 
 if __name__ == "__main__":
@@ -40,3 +55,5 @@ if __name__ == "__main__":
 
     bot = JobApply(data)
     bot.login_linkedin()
+    time.sleep(5)
+    bot.job_search()
